9da93e8e86745da80a3cfc24cfa84a1de071bd6d
Nbrthx <78901899+Nbrthx@users.noreply.github.com>
2021-07-18 09:50:25 +0700

    Add files via upload

---
+++ Raveling/src/com/niberthix/Raveling/PlayerLevelManager.java
@@ -0,0 +1,27 @@
+package com.niberthix.Raveling;
+
+public class PlayerLevelManager {
+	private int level;
+	private int xp;
+	
+	public PlayerLevelManager(int level, int xp) {
+		this.level = level;
+		this.xp = xp;
+	}
+	
+	public int getLevel() {
+		return level;
+	}
+	
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	public int getXp() {
+		return xp;
+	}
+	
+	public void setXp(int xp) {
+		this.xp = xp;
+	}
+}
---
+++ Raveling/src/com/niberthix/Raveling/plTabCompleter.java
@@ -0,0 +1,16 @@
+package com.niberthix.Raveling;
+
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+public class plTabCompleter implements TabCompleter {
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cde, String arg, String[] args) {
+
+    	return null;
+    }
+}
